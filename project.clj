9d796a44b7e056058ac0cae18106a799f1efd2d3
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-11-30 18:08:12 +0100

    [DEPS]

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(def hadoop-version "2.7.3")
-(def hbase-version "1.1.3")
+(def hadoop-version "2.7.4")
+(def hbase-version "1.1.7")
 
-(defproject luhhujbb/hbase-admin "0.1.10"
+(defproject luhhujbb/hbase-admin "0.1.11"
   :description "A library to administer hbase"
   :url "https://github.com/luhhujbb/clj-hbase-admin"
   :license {:name "Eclipse Public License"
